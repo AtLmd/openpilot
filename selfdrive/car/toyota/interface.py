6ebf3c3bd6d42ed0c41335e70b38a5def3d77f21
AtLmd <44336271+AtLmd@users.noreply.github.com>
2022-05-17 21:08:59 +0200

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -168,9 +168,13 @@ class CarInterface(CarInterfaceBase):
         ret.lateralTuning.indi.timeConstantV = [1, 3, 4.5]
         ret.lateralTuning.indi.actuatorEffectivenessBP = [18, 22, 26]
         ret.lateralTuning.indi.actuatorEffectivenessV = [9, 12, 15]
-        ret.steerActuatorDelay = 0.42 - 0.2
+        ret.steerActuatorDelay = 0.42
       else:
-        set_lat_tune(ret.lateralTuning, lat_params, LatTunes.PID_D)
+        ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[.028], [.0012]] #birdman6450#7399's Corolla 2020 PIF Tune 
+        ret.lateralTuning.pid.kdV = [0.]
+        ret.lateralTuning.pid.kf = 0.000153263811757641
+        ret.lateralTuning.pid.newKfTuned = True
+        ret.steerActuatorDelay = 0.48
 
     elif candidate in [CAR.LEXUS_ES_TSS2, CAR.LEXUS_ESH_TSS2]:
       stop_and_go = True
