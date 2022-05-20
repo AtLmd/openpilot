f5dfd639eb9fe609615ac7be7e5e5c76abef90ee
AtLmd <44336271+AtLmd@users.noreply.github.com>
2022-05-20 15:50:40 +0200

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -146,7 +146,7 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.indi.timeConstantV = [1, 3, 4.5]
       ret.lateralTuning.indi.actuatorEffectivenessBP = [18, 22, 26]
       ret.lateralTuning.indi.actuatorEffectivenessV = [9, 12, 15]
-      ret.steerActuatorDelay = 0.42 - 0.2
+      ret.steerActuatorDelay = 0.42
 
     elif candidate in (CAR.LEXUS_ES_TSS2, CAR.LEXUS_ESH_TSS2, CAR.LEXUS_ESH):
       stop_and_go = True
